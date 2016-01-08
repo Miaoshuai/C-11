7af1afcabdb786bb7b1f64b253cef6141e902b9c
miaoshua <945970809@qq.com>
2016-01-08 14:46:02 +0800

    unique_ptr管理动态数组

---
+++ smart_poiter/udelete.cpp
@@ -0,0 +1,18 @@
+/*======================================================
+    > File Name: udelete.cpp
+    > Author: MiaoShuai
+    > E-mail:  
+    > Other :  
+    > Created Time: 2016年01月08日 星期五 14时44分46秒
+ =======================================================*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<memory>
+
+int main(int argc,char **argv)
+{
+  std::unique_ptr<int[]> up(int[10]);
+  return 0;
+}
